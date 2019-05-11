98a30289081d66708c42d461c4ff7690dcf4172e
Redemax <redemax.2013@mail.ru>
2019-05-12 00:49:56 +0600

    Начало историй

---
+++ AppName/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppName
+//
+//  Created by Redemax on 5/12/19.
+//  Copyright © 2019 Alisher Altore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
